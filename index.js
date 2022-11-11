cd60d7ca3211c5c969e27893d94745a027ec5acd
Josue Martinez <josue.mart@jmcsoluciones.com>
2022-11-11 17:36:08 -0600

    Se agregan los archivos iniciales del ejercicio

---
+++ index.js
@@ -0,0 +1,13 @@
+var nombre = "Esaú Josué Martínez Carrasco";
+var edad = 38; 
+var desarrollador = true; 
+var nacimiento = "16/09/1984"; 
+var libro = {
+    titulo: "Hábitos atómicos", 
+    autor: "Carl Port",
+    fecha: "25/12/2019",
+    url: "https://google.com"
+}
+var lista = new Array(nombre, edad, desarrollador, nacimiento, libro)
+
+console.log(lista)
